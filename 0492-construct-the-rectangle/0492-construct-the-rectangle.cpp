558a155cb2c19da681c95944ec7c3dc33a99867a
shetkari03 <163853946+shetkari03@users.noreply.github.com>
2024-09-25 22:13:31 +0530

    Time: 3 ms (49.16%), Space: 7.5 MB (54.99%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> constructRectangle(int area) {
         int l=area,b=1;
-        for(int i=1;i<=area;i++){
+        for(int i=1;i<=sqrt(area);i++){
             if(area%i==0){
                 if(abs(area/i-i)<abs(l-b)){
                     l=max(area/i,i);
