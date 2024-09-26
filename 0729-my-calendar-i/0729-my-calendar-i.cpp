0d6b3337349c6faf9e50e363f44c6dbac4bc5d93
shetkari03 <163853946+shetkari03@users.noreply.github.com>
2024-09-26 18:49:33 +0530

    Time: 100 ms (36.35%), Space: 42.6 MB (69.74%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,16 +1,17 @@
 class MyCalendar {
 public:
-        vector<pair<int,int>>arr;
+        set<pair<int,int>>arr;
     MyCalendar() {
     }
     
     bool book(int start, int end) {
             for(auto it:arr){
+                if(it.first>=end)break;
                 if(it.first<end && start<=it.first)return false;
                 if(start>=it.first && end<it.second)return false;
                 if(start<it.second && end>=it.second)return false;
             }
-            arr.push_back({start,end});
+            arr.insert({start,end});
             return true;
 
     }
