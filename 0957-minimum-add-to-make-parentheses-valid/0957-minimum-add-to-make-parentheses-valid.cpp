f71ed1516ed8091a55e731cd462d29d3ff0cf4b7
shetkari03 <163853946+shetkari03@users.noreply.github.com>
2024-10-09 07:59:41 +0530

    Time: 3 ms (42.94%), Space: 7.8 MB (81.46%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        // stack<char>st;
-        // for(int i=0;i<s.size();i++){
-        //     if(st.empty()==true){
-        //         st.push(s[i]);
-        //         continue;
-        //     }
-        //     else{
-        //         char ch=st.top();
-        //         if(ch=='(' && s[i]==')'){
-        //             st.pop();
-        //         }
-        //         else st.push(s[i]);
-        //     }
-        // }
-        // return st.size();
         stack<char>st;
-        for(auto it:s){
-            if(it=='(')st.push(it);
+        for(int i=0;i<s.size();i++){
+            if(st.empty()==true){
+                st.push(s[i]);
+                continue;
+            }
             else{
-                if(!st.empty() && st.top()=='('){
+                char ch=st.top();
+                if(ch=='(' && s[i]==')'){
                     st.pop();
                 }
-                else st.push(it);
+                else st.push(s[i]);
             }
         }
         return st.size();
+        // stack<char>st;
+        // for(auto it:s){
+        //     if(it=='(')st.push(it);
+        //     else{
+        //         if(!st.empty() && st.top()=='('){
+        //             st.pop();
+        //         }
+        //         else st.push(it);
+        //     }
+        // }
+        // return st.size();
     }
 };
