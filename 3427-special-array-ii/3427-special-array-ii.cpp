cff5088a1183eaa64255fb8358151d9ae29f7cc4
shetkari03 <163853946+shetkari03@users.noreply.github.com>
2024-10-19 17:27:57 +0530

    Time: 227 ms (21.32%), Space: 153 MB (5.04%) - LeetHub

---
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        map<int,int>mpp;
+        int ct=0,n=nums.size();
+        for(int i=1;i<n;i++){
+            if(nums[i]%2==nums[i-1]%2){
+                ct++;
+            }
+            mpp[i]=ct;
+        }
+        vector<bool>ans;
+        for(auto it:queries){
+            int i=it[0],j=it[1];
+            if(mpp[i]==mpp[j])ans.push_back(true);
+            else ans.push_back(false);
+        }
+        return ans;
+    }
+};
