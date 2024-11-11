c6eb58ba457fd283078c8184d518b761afcf2859
shetkari03 <163853946+shetkari03@users.noreply.github.com>
2024-11-11 07:36:45 +0530

    Time: 1147 ms (5.18%), Space: 28.6 MB (61.98%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     bool primeSubOperation(vector<int>& nums) {
-        vector<bool>prime(1001,false);
+        vector<int>prime;
         for(int i=2;i<=1000;i++){
             int ct=0;
             for(int j=1;j<=1000;j++)if(i%j==0)ct++;
-            if(ct==2)prime[i]=true;
+            if(ct==2)prime.push_back(i);
         }
-        int prev=-1;
+        int m=prime.size();
+        int prev=0;
         for(int i=0;i<nums.size();i++){
-            int d=0;
-            for(int j=2;j<nums[i];j++){
-                if(prime[j]==true && nums[i]-j>prev)d=j;
+            int left=0,right=m-1;
+            while(left<=right){
+                int mid=(left+right)/2;
+                int d=nums[i]-prime[mid];
+                if(d>prev)left=mid+1;
+                else right=mid-1;
             }
-            nums[i]=nums[i]-d;
+            int a=min(left,right),b=max(left,right);
+            if(b>=0 && b<m && nums[i]-prime[b]>prev){
+                nums[i]-=prime[b];
+            }
+            else if(a>=0 && a<m && nums[i]-prime[a]>prev)nums[i]-=prime[a];
             prev=nums[i];
         }
         for(int i=1;i<nums.size();i++)if(nums[i]<=nums[i-1])return false;
