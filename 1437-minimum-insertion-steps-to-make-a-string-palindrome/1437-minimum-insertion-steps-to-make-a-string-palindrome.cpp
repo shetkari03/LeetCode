4db7e9cb9f715faf390662f550ce114b561c940b
shetkari03 <163853946+shetkari03@users.noreply.github.com>
2024-11-18 18:58:03 +0530

    Time: 17 ms (94.3%), Space: 8.2 MB (94.19%) - LeetHub

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.size(),m=text2.size();
+        vector<int>dp(m+1,0);
+        for(int i=1;i<=n;i++){
+            int prev=dp[0];
+            for(int j=1;j<=m;j++){
+                int k=dp[j];
+                if(text1[i-1]==text2[j-1]){
+                    dp[j]=1+prev;
+                }
+                else{
+                    dp[j]=dp[j]>dp[j-1]?dp[j]:dp[j-1];
+                }
+                prev=k;
+            }
+        }
+        return dp[m];
+    }
+    int minInsertions(string s) {
+        string s1=s;
+        reverse(s1.begin(),s1.end());
+        return s.size()-longestCommonSubsequence(s,s1);
+    }
+};
