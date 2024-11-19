3bf13e51d28d78cbb1e152db24158166bda008b4
shetkari03 <163853946+shetkari03@users.noreply.github.com>
2024-11-19 15:26:55 +0530

    Time: 1958 ms (6.73%), Space: 649.7 MB (6.79%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 bool func(string s1,string s2){
-    if(s1.size()<s2.size())return func(s2,s1);
+    //if(s1.size()<s2.size())return func(s2,s1);
     int i=0,j=0,n=s1.size(),m=s2.size();
     if(n-m!=1)return false;
     while(i<n && j<m){
@@ -14,19 +14,18 @@ bool func(string s1,string s2){
     return j==m;
 }
     int longestStrChain(vector<string>& words) {
-        int n=words.size();
+        int n=words.size(),ans=1;
         sort(words.begin(),words.end(),[](auto a,auto b){
             return a.size()<b.size();
         });
         vector<int>dp(n,1);
         for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
-                if(func(words[j],words[i]))dp[i]=max(dp[i],1+dp[j]);
+                if(func(words[i],words[j]))dp[i]=max(dp[i],1+dp[j]);
             }
+            ans=ans>dp[i]?ans:dp[i];
         }
-        int ans=0;
-        for(auto it:dp)cout<<it<<" ";
-        for(auto it:dp)ans=ans>it?ans:it;
+        //for(auto it:dp)ans=ans>it?ans:it;
         return ans;
     }
 };
